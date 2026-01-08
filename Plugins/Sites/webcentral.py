7dbbe1f4a9913e6cd34bbfb101eb197b2f99d93f
•_• <abhinavaditya035@gmail.com>
2026-01-08 14:27:17 +0530

    Update webcentral.py

--- Plugins/Sites/webcentral.py
+++ Plugins/Sites/webcentral.py
@@ -23,7 +23,9 @@ class WebCentralAPI:
         self.base_url = "https://weebcentral.com"
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Referer": "https://weebcentral.com/"
+            "Referer": "https://weebcentral.com/",
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5'
         }
 
     async def __aenter__(self):
